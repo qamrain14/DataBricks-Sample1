053dc47c1f07d9722cc1ba247df656718a19a825
qamrain14 <mohd.qamrain@hotmail.com>
2026-04-14 00:51:28 -0500

    2- Baseline before - Domain specific data

--- api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+305e66b3a00b22a9f07be656663b47b3bac323d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6ba06f2c0a8fb482eefc0d9fd9a28626aef8d71")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
